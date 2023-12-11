9229b090e2d6a9fa16bf7d7b15af9611edd8579f
Dai-Wenxun <wxDai2001@gmail.com>
2023-12-12 00:36:23 +0800

    no debug

--- train_lora.py
+++ train_lora.py
@@ -55,7 +55,7 @@ def main(
         beta_schedule: str = "scaled_linear",
         num_train_timesteps: int = 1000,
         clip_sample: bool = False,
-        is_debug: bool = True,
+        is_debug: bool = False,
         mixed_precision: str = 'fp16'
 ) -> None:
 
